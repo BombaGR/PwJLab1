7184b36655ab456f18a327edf7f3dee9979f52df
Mateusz Wojcik <w.matiu@gmail.com>
2018-02-21 14:35:47 +0100

    class KeyCard edit

--- src/KeyCard.java
+++ src/KeyCard.java
@@ -1,2 +1,30 @@
-public class KeyCard {
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class KeyCard
+{
+    List<String> keyList = new ArrayList<String>();
+
+    public void readKeyCard(String fileName)
+    {
+        Path sciezka = Paths.get(fileName);
+        try
+        {
+            keyList = (ArrayList) Files.readAllLines(sciezka);
+        }
+        catch (IOException ex)
+        {
+            System.out.println("No file!");
+        }
+    }
+
+    public void print()
+    {
+        for(int i = 0; i<keyList.size();i++)
+        {
+            System.out.println(keyList.get(i));
+        }
+    }
 }
